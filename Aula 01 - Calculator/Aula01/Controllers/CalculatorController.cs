bf335ed12a160a04da5c9fbcc631f9125fd0a334
Frederico Ribeiro <BSI90702@BBO.BR>
2018-11-20 18:00:19 -0200

    Starting development in Rest

---
+++ Aula 01 - Calculator/Aula01/Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula01.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Calculator")]
+    public class CalculatorController : Controller
+    {
+        // GET: api/Calculator/5
+        [HttpGet("{firstNumber}/{secondNumber}", Name = "Sum")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+                return decimalValue;
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double doubleValue;
+
+            bool isNumeric = double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out doubleValue);
+
+            return isNumeric;
+        }
+    }
+}
