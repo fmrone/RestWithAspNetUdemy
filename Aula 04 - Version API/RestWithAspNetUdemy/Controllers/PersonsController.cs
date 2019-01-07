661f1077aa015f07fdbf8827b4c4dec5e946047e
Frederico Mateus Ribeiro <fmrone@gmail.com>
2019-01-07 19:30:03 -0200

    adding aula04 - versining api

---
+++ Aula 04 - Version API/RestWithAspNetUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using RestWithAspNetUdemy.Model;
+using RestWithAspNetUdemy.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNetUdemy.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonsController : Controller
+    {
+        private IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if (person == null)
+                return NotFound();
+
+            return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Person person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            return new OkObjectResult(_personService.Create(person));
+        }
+
+        // PUT api/values/
+        [HttpPut]
+        public IActionResult Put([FromBody]Person person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            return new OkObjectResult(_personService.Update(person));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Aula 04 - Version API/RestWithAspNetUdemy/Services/Implementations/PersonService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using RestWithAspNetUdemy.Model;
+using RestWithAspNetUdemy.Model.Context;
+
+namespace RestWithAspNetUdemy.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+        private MySqlContext _context;
+
+        public PersonService(MySqlContext context)
+        {
+            _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+
+                return person;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(long id)
+        {
+            var _person = _context.Persons.SingleOrDefault(q => q.Id == id);
+            
+            try
+            {
+                if (_person == null)
+                    _context.Persons.Remove(_person);
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(q => q.Id == id);
+        }
+
+        public Person Update(Person person)
+        {
+            var _person = _context.Persons.SingleOrDefault(q => q.Id == person.Id);
+            if (_person == null)
+                return new Person();
+
+            try
+            {
+                _context.Entry(_person).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+
+                return person;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
